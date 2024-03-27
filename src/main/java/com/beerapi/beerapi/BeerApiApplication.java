a9cdea7477dd6a37544aa0f26484d24e963be74a
Alex Milovanov <alex.milovanov.mb@gmail.com>
2024-03-27 15:31:11 +0800

    Basic spring boot config

---
+++ src/main/java/com/beerapi/beerapi/BeerApiApplication.java
@@ -0,0 +1,13 @@
+package com.beerapi.beerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beerapi/beerapi/BeerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerapi.beerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
